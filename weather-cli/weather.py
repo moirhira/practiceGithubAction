7fb37b31956519d8b866560c0c23e0e6832d381c
moirhira <ighermohamed@gmail.com>
2026-07-16 15:20:05 +0100

    ex07: formate the projectv3

--- weather-cli/weather.py
+++ weather-cli/weather.py
@@ -5,9 +5,8 @@ from config import API_KEY, BASE_URL
 def get_weather(city):
     if not API_KEY:
         raise RuntimeError("Missing API_KEY")
-    r = requests.get(
-        BASE_URL, params={"q": city, "appid": API_KEY, "units": "metric"}, timeout=10
-    )
+    params = {"q": city, "appid": API_KEY, "units": "metric"}
+    r = requests.get(BASE_URL, params, timeout=10)
     if r.status_code == 401:
         raise RuntimeError("Invalid API key")
     if r.status_code == 404:
